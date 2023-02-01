6eb7867b141acad552031397bbbb90409a8ef336
linocesar <souzaces@gmail.com>
2023-02-01 00:51:07 -0300

    add compressão jpeg

--- full_pistola.sh
+++ full_pistola.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 OUTPUT="profiling.txt"
+DPI=300
 
 if [ -f $OUTPUT ]; then
 	rm $OUTPUT
@@ -84,13 +85,19 @@ echo -e "$TABNAME: Analisando o output do processo, ilustrado na figura  abaixo,
 
 sed -i "s/DataFrame/$TABNAME/g" $FILE
 
-wkhtmltopdf $FILE $TABNAME."pdf"
 
-pdftoppm -singlefile -x 100 -y 50 -rx 300 -ry 300 -W 1470 -H 1240 -cropbox $TABNAME."pdf" $TABNAME -tiff
+wkhtmltopdf --background -q -d $DPI $FILE $TABNAME."pdf"
+
+echo -e $TABNAME."pdf gerado com sucesso!"
+
+pdftoppm -singlefile -q -tiffcompression jpeg -x 100 -y 50 -rx $DPI -ry $DPI -W 1470 -H 1240 -cropbox $TABNAME."pdf" $TABNAME -tiff
+
+echo -e $TABNAME."tif gerado com sucesso!\n"
 
 done
 
 DIR="output"
 
-mv *.tif $DIR
+#mv *.tif $DIR
 
+#pdftk *.pdf profiling.pdf
